9fc9650f74241218a64e29566a334c29a5217984
kimtorres2005 <kimtorres2005@gmail.com>
2023-06-22 11:31:22 -0500

    X added to the hamburger menu on the portal home

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -17,14 +17,50 @@ function updateDateTime() {
 }
 
 // Hamburger Menu
+let menuExpanded = false;
+
 function toggleMenu() {
-    document.getElementsByClassName("nav-ul")[0].classList.toggle("responsive");
+    const navUl = document.getElementsByClassName("nav-ul")[0];
+    const menuSymbol = document.getElementById("menu-symbol");
+
+    if (!menuExpanded) {
+        navUl.classList.add("responsive");
+        menuSymbol.innerHTML = "&#x2716";
+    } else {
+        navUl.classList.remove("responsive");
+        menuSymbol.innerHTML = "Menu"; 
+    }
+
+    menuExpanded = !menuExpanded; 
 }
 
-window.addEventListener('DOMContentLoaded', function() {
+
+window.addEventListener('DOMContentLoaded', function () {
     var navUl = document.getElementById('nav-ul');
     navUl.classList.toggle('responsive');
 })
+
+// Page Visits
+function incrementPageVisits() {
+    let pageVisits = localStorage.getItem('pageVisits');
+    if (pageVisits) {
+        pageVisits = parseInt(pageVisits) + 1;
+    } else {
+        pageVisits = 1;
+    }
+    localStorage.setItem('pageVisits', pageVisits.toString());
+}
+
+function getPageVisits() {
+    let pageVisits = localStorage.getItem('pageVisits');
+    return pageVisits ? parseInt(pageVisits) : 0;
+}
+
+incrementPageVisits();
+
+let pageVisitsCount = getPageVisits();
+document.querySelector('#sectionTwo ul li:nth-child(3)').textContent = 'Page Visits: ' + pageVisitsCount;
+
 
 copyNameState();
 updateDateTime();
