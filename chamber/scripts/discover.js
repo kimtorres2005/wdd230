fd3eda6940a5c06d003dfface9df1fde874ce29a
kimtorres2005 <kimtorres2005@gmail.com>
2023-06-25 22:18:24 -0500

    javascipt correction

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,10 +1,11 @@
 const msToDays = 86400000;
 const theDateToday = new Date();
-const lastVisit = new Date(localStorage.getItem("lastVisit-ls"));
+let lastVisit = new Date(localStorage.getItem("lastVisit-ls"));
 
 if (!lastVisit || isNaN(lastVisit)) {
     // User's first visit or invalid stored date
     document.querySelector(".days").textContent = "Welcome! Let us know if you have any questions.";
+    lastVisit = null; // Reset last visit to null
 } else {
     const timeDiff = theDateToday - lastVisit;
     const daysPassed = Math.floor(timeDiff / msToDays);
