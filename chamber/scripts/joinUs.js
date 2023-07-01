492d68653cff4b754c8f40e5f6e61e838b1dc7a0
kimtorres2005 <kimtorres2005@gmail.com>
2023-06-30 22:00:11 -0500

    java change

--- chamber/scripts/joinUs.js
+++ chamber/scripts/joinUs.js
@@ -5,3 +5,18 @@ function setFormLoadTime() {
     document.querySelector("#formLoadTime").value = currentDate.toISOString();
 }
 window.onload = setFormLoadTime;
+
+// Info Link Expand/Collapse
+function setupInfoLinks() {
+    const infoLinks = document.querySelectorAll('.info-link');
+
+    infoLinks.forEach((link) => {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+            const infoContainer = this.closest('.info-container');
+            const infoContent = infoContainer.querySelector('.info-content');
+            infoContent.classList.toggle('expanded');
+        });
+    });
+}
+setupInfoLinks();
