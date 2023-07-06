2dca9e5c7f16141822d8575d4e8c310951179912
kimtorres2005 <kimtorres2005@gmail.com>
2023-07-06 14:17:08 -0500

    FETCH API

--- week 05/scripts/prophets.js
+++ week 05/scripts/prophets.js
@@ -2,10 +2,13 @@ const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophet
 const cards = document.querySelector('#cards');
 
 async function getProphetData() {
-    const response = await fetch(url);
-    const data = await response.json();
-    // console.table(data.prophets); // temporary testing of data retreival
-    displayProphets(data.prophets);
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        displayProphets(data.prophets);
+    } catch (error) {
+        console.log('Error fetching prophet data:', error);
+    }
 }
 
 getProphetData();
@@ -15,17 +18,26 @@ const displayProphets = (prophets) => {
         const card = document.createElement('section');
         const fullName = document.createElement('h2');
         const portrait = document.createElement('img');
+        const details = document.createElement('div');
+        const title = document.createElement('p');
+        const birthdate = document.createElement('p');
 
         fullName.textContent = `${prophet.name.first} ${prophet.name.last}`;
+        title.textContent = `Title: ${prophet.title}`;
+        birthdate.textContent = `Birthdate: ${prophet.birthdate}`;
 
         portrait.setAttribute('src', prophet.imageurl);
         portrait.setAttribute('alt', `Portrait of ${prophet.name.last}`);
         portrait.setAttribute('loading', 'lazy');
-        portrait.setAttribute('width', '340');
-        portrait.setAttribute('height', '440');
+        portrait.setAttribute('width', '320');
+        portrait.setAttribute('height', '240');
+
+        details.appendChild(title);
+        details.appendChild(birthdate);
 
         card.appendChild(fullName);
         card.appendChild(portrait);
+        card.appendChild(details);
 
         cards.appendChild(card);
     });
