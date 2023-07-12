a912bfa4ba217430b14f0109440caecd6471bf82
Kimberly Torres <kimtorres2005@gmail.com>
2023-07-11 22:59:19 -0500

    changes to html

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -68,37 +68,85 @@ function topBanner() {
     }
 }
 
-// Function to fetch weather data from OpenWeatherMap API
-function fetchWeatherData() {
-    const apiKey = '6d25f33bb1267ba71d0fa8d39306cadc';
-    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=Colmesneil,us&appid=${apiKey}&units=imperial`;
+const currentTemp = document.querySelector("#temp");
+const captionDesc = document.querySelector("figcaption");
+const humidityValue = document.querySelector("#humidity-value");
+const forecastList = document.querySelector("#forecast-list");
+const city = "Colmesneil,us";
+const units = "imperial";
+const apiKey = "6d25f33bb1267ba71d0fa8d39306cadc";
 
-    $.ajax({
-        url: apiUrl,
-        method: 'GET',
-        success: function (response) {
-            const forecastList = response.list.slice(0, 3); // Get the first three forecasts
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&APPID=${apiKey}`;
+const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=${units}&cnt=24&APPID=${apiKey}`;
 
-            // Update the forecast elements
-            forecastList.forEach((forecast, index) => {
-                const temperature = forecast.main.temp;
-                const description = forecast.weather[0].description;
+async function apiFetch() {
+    try {
+        const [weatherResponse, forecastResponse] = await Promise.all([
+            fetch(url),
+            fetch(forecastUrl),
+        ]);
 
-                const temperatureElement = document.querySelector(`#temp${index + 1}`);
-                const descriptionElement = document.querySelector(`#desc${index + 1}`);
+        if (weatherResponse.ok && forecastResponse.ok) {
+            const weatherData = await weatherResponse.json();
+            const forecastData = await forecastResponse.json();
 
-                temperatureElement.textContent = Math.round(temperature) + '°F';
-                descriptionElement.textContent = description;
-            });
-        },
-        error: function (error) {
-            console.log('Error fetching weather data:', error);
+            console.log(weatherData); // for testing purposes
+            console.log(forecastData); // for testing purposes
+
+            displayWeather(weatherData);
+            displayForecast(forecastData);
+        } else {
+            throw Error("Weather data could not be retrieved.");
         }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayWeather(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = capitalizeWords(weatherData.weather[0].description);
+
+    const weatherImage = document.createElement("img");
+    weatherImage.setAttribute("src", iconsrc);
+    weatherImage.setAttribute("alt", desc);
+
+    const weatherIconContainer = document.querySelector("#weather-icon");
+    weatherIconContainer.appendChild(weatherImage);
+
+    captionDesc.textContent = desc;
+    humidityValue.textContent = `${weatherData.main.humidity}% humidity`;
+}
+
+function displayForecast(forecastData) {
+    const forecastItems = forecastData.list.filter((item, index) => index % 8 === 0); // Get one forecast item per day (every 8th item)
+
+    forecastItems.forEach((item) => {
+        const forecastDate = new Date(item.dt_txt);
+        const forecastDay = forecastDate.toLocaleDateString("en-US", { weekday: "short" });
+        const forecastTemp = item.main.temp.toFixed(0);
+        const forecastIcon = item.weather[0].icon;
+
+        const iconsrc = `https://openweathermap.org/img/w/${forecastIcon}.png`;
+
+        const listItem = document.createElement("li");
+        listItem.innerHTML = `
+      <img src="${iconsrc}" alt="Weather Icon">
+      <span>${forecastDay}</span>
+      <strong>${forecastTemp} &deg;F</strong>
+    `;
+
+        forecastList.appendChild(listItem);
     });
 }
 
-// Call the fetchWeatherData function to retrieve and update the weather forecast
-fetchWeatherData();
+function capitalizeWords(str) {
+    return str.replace(/\b\w/g, (match) => match.toUpperCase());
+}
+
+apiFetch();
 
 
 // // Form Validation
