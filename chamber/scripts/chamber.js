00a288b8d101a873ccfb8c520f18a83638732d61
Kimberly Torres <kimtorres2005@gmail.com>
2023-07-11 22:04:15 -0500

    weather changes

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -55,18 +55,51 @@ function setupInfoLinks() {
 }
 setupInfoLinks();
 
-// // Memo Banner
-// function topBanner() {
-//     let curDate = new Date();
-//     let dayOfWeek = curDate.getDay();
-//     if (dayOfWeek === 1 || dayOfWeek === 2) {
-//         let banner = document.createElement("div");
-//         banner.classList.add("banner");
-//         banner.innerText = '🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.';
-//         let body = document.body;
-//         body.insertBefore(banner, body.firstChild);
-//     }
-// }
+// Memo Banner
+function topBanner() {
+    let curDate = new Date();
+    let dayOfWeek = curDate.getDay();
+    if (dayOfWeek === 1 || dayOfWeek === 2) {
+        let banner = document.createElement("div");
+        banner.classList.add("banner");
+        banner.innerText = '🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.';
+        let body = document.body;
+        body.insertBefore(banner, body.firstChild);
+    }
+}
+
+// Function to fetch weather data from OpenWeatherMap API
+function fetchWeatherData() {
+    const apiKey = '6d25f33bb1267ba71d0fa8d39306cadc';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=Colmesneil,us&appid=${apiKey}&units=imperial`;
+
+    $.ajax({
+        url: apiUrl,
+        method: 'GET',
+        success: function (response) {
+            const forecastList = response.list.slice(0, 3); // Get the first three forecasts
+
+            // Update the forecast elements
+            forecastList.forEach((forecast, index) => {
+                const temperature = forecast.main.temp;
+                const description = forecast.weather[0].description;
+
+                const temperatureElement = document.querySelector(`#temp${index + 1}`);
+                const descriptionElement = document.querySelector(`#desc${index + 1}`);
+
+                temperatureElement.textContent = Math.round(temperature) + '°F';
+                descriptionElement.textContent = description;
+            });
+        },
+        error: function (error) {
+            console.log('Error fetching weather data:', error);
+        }
+    });
+}
+
+// Call the fetchWeatherData function to retrieve and update the weather forecast
+fetchWeatherData();
+
 
 // // Form Validation
 // function setFormLoadTime() {
@@ -91,5 +124,5 @@ setupInfoLinks();
 getCurrentDate();
 displayFooter();
 toggleMenu();
-// topBanner();
+topBanner();
 // setupInfoLinks();
